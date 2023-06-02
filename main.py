89aef4e8040fd3987fb8428e2d8d2991f15dc3d6
Shivam yadav <74147050+IncineratorR@users.noreply.github.com>
2023-06-02 21:34:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,63 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import requests
+from bs4 import BeautifulSoup
+import math
+
+# Load the pre-trained model and tokenizer
+model_path = './shakespeare_model'
+model_name = 'gpt2'
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+model = GPT2LMHeadModel.from_pretrained(model_path)
+
+# Generate Shakespearean text
+def generate_shakespearean_text(prompt):
+    input_ids = tokenizer.encode(prompt, return_tensors='pt')
+    output = model.generate(input_ids, max_length=100, num_return_sequences=1)
+    shakespearean_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    return shakespearean_text
+
+# Browse the internet and extract information
+def browse_internet(query):
+    url = "https://www.google.com/search?q=" + query  # Replace with appropriate URL for browsing
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Extract relevant information from the webpage using BeautifulSoup
+
+    # Process and display the extracted information in a Shakespearean style
+    # ...
+
+# Perform mathematical operations
+def perform_mathematical_operations(expression):
+    try:
+        if "factorial" in expression:
+            num = expression.replace("factorial", "").strip()
+            num = int(num)
+            result = math.factorial(num)
+            shakespearean_result = str(result)
+        else:
+            result = eval(expression)
+            shakespearean_result = str(result)
+    except (SyntaxError, TypeError, ValueError):
+        shakespearean_result = "Sorry, I couldn't compute the expression."
+
+    return shakespearean_result
+
+# Interactive conversation loop
+print("Welcome to the Shakespearean Chatbot!")
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "exit":
+        print("Fare thee well, kind interlocutor!")
+        break
+
+    if "browse" in user_input.lower():
+        query = user_input.lower().replace("browse", "").strip()
+        browse_internet(query)
+    elif "math" in user_input.lower():
+        expression = user_input.lower().replace("math", "").strip()
+        shakespearean_result = perform_mathematical_operations(expression)
+        print("Shakespeare: ", shakespearean_result)
+    else:
+        shakespearean_text = generate_shakespearean_text(user_input)
+        print("Shakespeare: ", shakespearean_text)
 
